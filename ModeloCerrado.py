8309a7b0084c1bd3bfad77b1280eec3451eed3f4
Daniel Banariba <banaribad@gmail.com>
2024-07-05 22:04:20 -0600

    Se creo una carpeta llamado DB donde contiene la base de datos y un fichero .py para conectar a la base de datos

--- ModeloCerrado.py
+++ ModeloCerrado.py
@@ -1,26 +1,8 @@
 import psycopg2
 from psycopg2 import Error
+from db.conectar import conectar
 
-# Función para conectar a la base de datos
-def conectar():
-    try:
-        # Replace placeholders with your actual credentials
-        host = "c5hilnj7pn10vb.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com"
-        dbname = "d5iadjk8hco3u9"
-        user = "uq1n0nfa18qpc"
-        password = "p35e6f1144021830e2e3e7253782e776b2359467969040c6288b7cef1718fdb81"
-
-        # Connect to the PostgreSQL database
-        conn = psycopg2.connect(
-            host=host,
-            dbname=dbname,
-            user=user,
-            password=password
-        )
-        return conn
-    except (Exception, psycopg2.Error) as error:
-        print("Error while connecting to PostgreSQL:", error)
-        return None
+conn = conectar()
 
 # Función para calcular promedios iniciales de calificaciones de las películas desde la base de datos
 def calcular_promedios(conn):
